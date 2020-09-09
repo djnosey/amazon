db23f3f0254c8c8381dd3356da7f126cdf6a3a75
steven payne <djnosey@hotmail.co.uk>
2020-09-09 19:58:03 +0200

    first commit, homepage almost done, adding react router and working on checkout page 1:04

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDexw_iRoKj8VWAvM0YN7KINdzbyqrS7zw",
+    authDomain: "clone-d29c9.firebaseapp.com",
+    databaseURL: "https://clone-d29c9.firebaseio.com",
+    projectId: "clone-d29c9",
+    storageBucket: "clone-d29c9.appspot.com",
+    messagingSenderId: "839358073063",
+    appId: "1:839358073063:web:7b998454be9efd8e7c3a42",
+    measurementId: "G-MJLG8EPKRH"
+  };
